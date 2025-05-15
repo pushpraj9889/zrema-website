d7f21ddfec4e1214e79d06c956ec461ecaf082f2
pushpraj9889 <pushprajsingh5821@gmail.com>
2025-05-15 10:58:27 +0530

    product page changes

--- src/pages/ViewcartPage/index.jsx
+++ src/pages/ViewcartPage/index.jsx
@@ -181,22 +181,24 @@ export default function Viewcart() {
                         <h3 className="text-base font-medium text-gray-900">
                           {item.name}
                         </h3>
+                        <div className="text-right">
+                          <div className="flex items-center">
+                            {/* <span className="line-through text-sm text-gray-500 mr-2">
+                              Rs. {item.mrp.toFixed(2)}
+                            </span> */}
+                            <span className="text-lg font-medium text-gray-900">
+                              Rs. {item.mrp.toFixed(2)}
+                            </span>
+                          </div>
+                        </div>
                       </div>
+
                       <p className="mt-1 text-sm text-gray-500">
                         {item.color} / {item.material} / {item.size}
                       </p>
                     </div>
 
-                    <div className="flex flex-1 items-end justify-between mt-4">
-                      <div className="flex items-center">
-                        <button
-                          onClick={() => removeItem(item._id)}
-                          className="text-gray-500 hover:text-red-500"
-                        >
-                          <Trash2 size={18} />
-                        </button>
-                      </div>
-
+                    <div className="flex  items-end justify-between  items-center mt-2">
                       <div className="flex items-center">
                         <div className="flex items-center border border-gray-300 rounded">
                           <button
@@ -215,17 +217,14 @@ export default function Viewcart() {
                             <ChevronUp size={16} />
                           </button>
                         </div>
-
-                        <div className="text-right">
-                          <div className="flex items-center">
-                            <span className="line-through text-sm text-gray-500 mr-2">
-                              Rs. {item.mrp.toFixed(2)}
-                            </span>
-                            <span className="text-lg font-medium text-gray-900">
-                              Rs. {item.mrp.toFixed(2)}
-                            </span>
-                          </div>
-                        </div>
+                      </div>
+                      <div className="flex items-center">
+                        <button
+                          onClick={() => removeItem(item._id)}
+                          className="text-gray-500 hover:text-red-500"
+                        >
+                          <Trash2 size={18} />
+                        </button>
                       </div>
                     </div>
                   </div>
